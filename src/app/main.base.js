92b1add821a9c017bed8d832a27f7562e40c13f3
Giovanni <appcounts47@gmail.com>
2023-09-23 22:46:11 +0200

    INTRODUCING NEW SPLASH SCREEN [beta]

--- src/app/main.base.js
+++ src/app/main.base.js
@@ -1,3 +1,14 @@
+const splashItem = document.querySelector('body .splash .audio-shader .waves');
+for (let i = 0; i < 300 / 6; i++) {
+  const value = Math.floor(Math.random() * (100 - 30)) + 30;
+
+  const wave = document.createElement('div');
+  wave.classList.add('wave');
+  wave.style.setProperty('--i', i);
+  wave.style.setProperty('--value', value);
+  splashItem.appendChild(wave);
+}
+
 window.addEventListener('load', () => {
   const splashScreen = document.querySelector('body .splash');
   if (splashScreen) {
